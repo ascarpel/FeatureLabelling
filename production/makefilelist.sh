5ad0fc4d657d408c1e8be96aad3763d9b5a65707
ascarpel <scarpell@apc.in2p3.fr>
2018-09-27 22:44:21 +0200

    production scripts

---
+++ production/makefilelist.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+listname=${1}
+name=${2}
+#config="/afs/cern.ch/work/a/ascarpel/private/FeatureLabelling/config.json"
+
+copydir="/eos/user/a/ascarpel/CNN/${name}/root/"
+#listname="filelist.txt"
+echo ${listname}
+echo ${copydir}
+
+if [  -f ${listname} ]; then
+  echo "${listname} will be erased and created again"
+  rm ${listname}
+  touch ${listname}
+fi
+
+for i in {0..5000} #enter here the range of run numbers you want to save to the OutputList, 633..1199
+do
+
+  filename="${name}_hist_${i}.root"
+
+  if [  -f $copydir"/"$filename ]; then
+    echo "add file file ${copydir}/${filename} to ${listname}"
+    echo ${copydir}/${filename}  >> ${listname}
+    #echo -i ${copydir}/${filename} -c ${config} >> ${listname}
+  fi
+
+done
